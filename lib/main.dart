add36504f5b86c7b941af18f644ff4d3d69d5279
zivdmagdy <ziadmagdy109@yahoo.com>
2026-02-04 05:51:10 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:api/views/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
